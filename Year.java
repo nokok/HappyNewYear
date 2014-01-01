ae37aba8284ae33ca73da28f3bd511984f909438
nokok <nokok.kz@gmail.com>
2014-01-01 13:41:43 +0900

    first commit

---
+++ Year.java
@@ -0,0 +1,148 @@
+class Year {
+    public static void main(String[] args) {
+        char[] C = {
+            new Year().happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))))), new Year().happy()),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy())))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy())))))), new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))),new Year().
+            happy()), new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy())))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()),new Year().happy(new Year().
+            happy(new Year().happy()))), new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy())))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy())))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(), new Year().happy(new Year().
+            happy(new Year().happy()))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy())))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(),new Year().happy(new Year().
+            happy())), new Year().happy(new Year().
+            happy(new Year().happy()))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy())))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))),new Year().
+            happy()), new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy())))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(), new Year().happy(new Year().
+            happy(new Year().happy()))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy())))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))))),new Year().happy()),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy())))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy())))))),new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy()))))))),new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy(new Year().happy(new Year().
+            happy()))))), new Year().happy()),};
+        System.out.println(C);
+    }
+    int happy(){return String.valueOf((new int[]{}).length).length();}char happy(int i){return(char)(i<<happy());}char happy(int i,int n){return(char)(i|n);}}
